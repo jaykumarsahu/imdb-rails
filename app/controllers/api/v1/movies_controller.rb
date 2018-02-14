88f1c57cec3ccbcfb8b71c0e85b39b7f5c42032d
Jay Sahu <jsahu.ror@gmail.com>
2018-02-15 03:20:57 +0530

    kk

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -9,13 +9,28 @@ class Api::V1::MoviesController < ApplicationController
   end
 
   def create
-    binding.pry
+    begin
+      movie = Movie.new(movie_params)
+      if movie.save
+        # binding.pry
+        # AttachmentWorker.perform_async(movie.id, image_params[:image])
+        render json: { status: "SUCCESS" }, status: :ok
+      else
+        render json: { status: "FAILED", error: movie.errors.full_messages.join("\n") }, status: :ok
+      end
+    rescue Exception => e
+      render json: { status: "FAILED", error: e.message }, status: :ok
+    end
   end
 
   private
 
-  def movies_params
+  def movie_params
     params.require(:movie).permit(:name, :director, :release_date, :image)
+  end
+
+  def image_params
+    params.require(:movie).permit(:image)
   end
 
 end
