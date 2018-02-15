d2b008e8c9f917bce9685e05c3997c19a4134716
jay <jsahu.ror@gmail.com>
2018-02-15 20:00:15 +0530

    in pr

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MoviesController < ApplicationController
-
+  before_action :init_movie, only: :show
 
   def index
     movies = Movie.json_data
@@ -8,12 +8,20 @@ class Api::V1::MoviesController < ApplicationController
     end
   end
 
+  def show
+    sleep(2)
+    if @movie.present?
+      render json: { status: "SUCCESS", movie: @movie.build_json}, status: :ok
+    else
+      render json: { status: "FAILED", error: "Movie not found." }, status: :ok
+    end
+  end
+
   def create
     begin
       movie = Movie.new(movie_params)
       if movie.save
-        # binding.pry
-        # AttachmentWorker.perform_async(movie.id, image_params[:image])
+        movie.image.attach(image_params[:image]) if image_params[:image].present?
         render json: { status: "SUCCESS" }, status: :ok
       else
         render json: { status: "FAILED", error: movie.errors.full_messages.join("\n") }, status: :ok
@@ -25,8 +33,12 @@ class Api::V1::MoviesController < ApplicationController
 
   private
 
+  def init_movie
+    @movie =  Movie.find_by(id: params[:id])
+  end
+
   def movie_params
-    params.require(:movie).permit(:name, :director, :release_date, :image)
+    params.require(:movie).permit(:name, :director, :release_date)
   end
 
   def image_params
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,7 @@ class Movie < ApplicationRecord
 
   validates :name, :release_date, :director, presence: :true
   validate :should_be_uniq_by_name_and_release_date
+  validate :validate_release_date
 
   scope :by_name_and_director, ->(name, director) { where(name: name.downcase, director: director.downcase) }
 
@@ -16,6 +17,10 @@ class Movie < ApplicationRecord
     image.service_url if image.attached?
   end
 
+  def build_json
+    self.decorate.as_json(methods: [:image_url, :name, :release_date, :director], only: [:id])
+  end
+
   private
 
   def downcase_fields
@@ -26,6 +31,12 @@ class Movie < ApplicationRecord
   def should_be_uniq_by_name_and_release_date
     if Movie.by_name_and_director(name, director).exists?
       errors[:base] << "Movie should be uniqe by name and release date."
+    end
+  end
+
+  def validate_release_date
+    if self.release_date.blank? || self.release_date < Date.parse("01/01/1900")
+      errors[:base] << "Please enter valid release date."
     end
   end
 end
