9fec9c81f484157f923000c0d332c646b3599a34
Jay Sahu <jsahu.ror@gmail.com>
2018-02-15 20:00:15 +0530

    in pr

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :products, only: [:index]
-      resources :movies, only: [:index, :create]
+      resources :movies, only: [:index, :create, :show]
       resources :sessions, only: [] do
         post :login, on: :collection
       end
